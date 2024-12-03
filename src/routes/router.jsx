1c63398c08d60385ba0d44e5f7d45e4e52bde951
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-03 20:06:34 +0600

    basic project setup and  add manrope font, tailwind css, daisyui, react router dom

---
+++ src/routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ut, laudantium cum labore velit placeat natus soluta repudiandae nostrum qui.</div>,
+    },
+]);
+
+export default router;
